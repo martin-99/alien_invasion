ddb0d681703360896eef298efc1d6567beef7b7f
martin-99 <martinm199913@gmail.com>
2020-09-07 22:38:32 +0100

    Creating a new class Setting that will manage all settings that we will need for the game

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """Class that helps us store all setting neede for our game"""
+    def __init__(self):
+        """Initialize first settings"""
+
+        #Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
