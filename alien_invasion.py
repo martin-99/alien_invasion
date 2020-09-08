7111c5e56340900d1bd2eed9c8e26a17dafb3f97
martin-99 <martinm199913@gmail.com>
2020-09-08 17:19:21 +0100

    Adding moving functionality for our ship as well as ship speed in to our settings.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,11 +14,13 @@ class AlienInvasion:
         )
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
+        
       
     def run_game(self):
         """Start the main loop of the game"""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
            
     def _check_events(self):
@@ -26,6 +28,20 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key ==pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+                
+
+
+
     def _update_screen(self):
         """on every change update the screen"""
         self.screen.fill(self.settings.bg_color)
--- ship.py
+++ ship.py
@@ -9,8 +9,28 @@ class Ship:
         #Load the ship image and get its rect
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
+
+         #Getting instance of game settings
+        self.settings = ai_game.settings
+
         #Start each new ship from the middle bottom of the screen
         self.rect.midbottom= self.screen_rect.midbottom
+
+        self.x = float(self.rect.x)
+       
+
+        #Moving flag
+        self.moving_right = False
+        self.moving_left = False
+    
+    def update(self):
+        #Update ship's position based on the moving flag
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        elif self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        
+        self.rect.x = self.x
     def blitme(self):
         self.screen.blit(self.image,self.rect)
 
