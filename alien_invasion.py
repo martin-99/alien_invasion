6223c76cb887576022d71bf63c8c3acbe319ccc9
martin-99 <martinm199913@gmail.com>
2020-09-09 00:23:17 +0100

    Simplifying _check_events method by separating the functionality into two different

--- alien_invasion.py
+++ alien_invasion.py
@@ -29,15 +29,25 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key ==pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self.check_keydown_events(event)
+
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
+                self.check_keyup_events(event)
+                
+
+    def check_keydown_events(self,event):
+        """Respond to keypresess."""
+        if event.key ==pygame.K_RIGHT:
+                    self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+    
+    def check_keyup_events(self,event):
+        if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
+        elif event.key == pygame.K_LEFT:
                     self.ship.moving_left = False
+
                 
 
 
